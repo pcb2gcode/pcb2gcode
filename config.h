269a082e1289d5714bb97eabfb2108b0e36733d9
Patrick Birnzain <pbirnzain@users.sourceforge.net>
2010-09-24 23:11:08 +0200

    * set version to 1.0.0 * added example config file

--- config.h
+++ config.h
@@ -66,7 +66,7 @@
 #define PACKAGE_NAME "pcb2gcode"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "pcb2gcode 0.9.9"
+#define PACKAGE_STRING "pcb2gcode 1.0.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "pcb2gcode"
@@ -75,13 +75,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.9.9"
+#define PACKAGE_VERSION "1.0.0"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.9.9"
+#define VERSION "1.0.0"
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
