68c8ff0f447f4676b6664e42882e59d7f4b9773e
Patrick Birnzain <pbirnzain@users.sourceforge.net>
2010-11-26 14:39:14 +0100

    changed version to 1.0.7 for file release

--- config.h
+++ config.h
@@ -66,7 +66,7 @@
 #define PACKAGE_NAME "pcb2gcode"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "pcb2gcode 1.0.6"
+#define PACKAGE_STRING "pcb2gcode 1.0.7"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "pcb2gcode"
@@ -75,13 +75,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.6"
+#define PACKAGE_VERSION "1.0.7"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.0.6"
+#define VERSION "1.0.7"
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
