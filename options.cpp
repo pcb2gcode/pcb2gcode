pcb2gcode/pcb2gcode

commit c2a0133a4cfb1db85a4a500375de06f22086c800

a different approach to the basename setting
this has the same purpose, but another method than bkubicek's --basename
patch from
0004-milling-of-holes-using-a-small-mill-head-activated.patch

as the current patch queue already contains infrastructure to set the
individual output file names, this version just modifies those arguments
and leaves the rest of the code unchanged.

the methods used in this patch are none that i like, but i didn't find
a better way to feed additional arguments to the vm.

options.cpp
@@ -46,9 +46,10 @@ options::parse( int argc, char** argv )
 	// (--drill, --drill-diameter); see bug 3089930
 	int style = po::command_line_style::default_style & ~po::command_line_style::allow_guessing;
 
+	po::options_description generic;
+	generic.add(instance().cli_options).add(instance().cfg_options);
+
 	try {
-		po::options_description generic;
-		generic.add(instance().cli_options).add(instance().cfg_options);
 		po::store(po::parse_command_line(argc, argv, generic, style), instance().vm);
 	}
 	catch( std::logic_error& e ) {
@@ -61,6 +62,25 @@ options::parse( int argc, char** argv )
 	po::notify( instance().vm );
 
 	parse_files();
+
+	/*
+	 * this needs to be an extra step, as --basename modifies the default
+	 * values of the --...-output parameters
+	 */
+	string basename="";
+	if( instance().vm.count("basename"))
+	{
+		basename = instance().vm["basename"].as<string>()+"_";
+	}
+
+	const char *fake_basename_command_line[] = {"",
+		"--front-output", (basename+"front.ngc").c_str(),
+		"--back-output", (basename+"back.ngc").c_str(),
+		"--outline-output", (basename+"outline.ngc").c_str(),
+		"--drill-output", (basename+"drill.ngc").c_str()
+	};
+	po::store(po::parse_command_line(9, (char**)fake_basename_command_line, generic, style), instance().vm);
+	po::notify(instance().vm);
 }
 
 string
@@ -130,6 +150,7 @@ options::options() : cli_options("command line only options"),
 		("dpi",      po::value<int>()->default_value(1000),   "virtual photoplot resolution")
 		("mirror-absolute",      po::value<bool>()->zero_tokens(),   "mirror back side along absolute zero instead of board center\n")
 
+		("basename",      po::value<string>(), "prefix for default output file names")
 		("front-output", po::value<string>()->default_value("front.ngc"), "output file for front layer")
 		("back-output", po::value<string>()->default_value("back.ngc"), "output file for back layer")
 		("outline-output", po::value<string>()->default_value("outline.ngc"), "output file for outline")

commit 1371be7aa0d852cffee2f179d6d85ffb4269ea40

c2a0133a broke the output naming for me, fixed. minor refactoring

options.cpp
@@ -73,13 +73,15 @@ options::parse( int argc, char** argv )
 		basename = instance().vm["basename"].as<string>()+"_";
 	}
 
-	const char *fake_basename_command_line[] = {"",
-		"--front-output", (basename+"front.ngc").c_str(),
-		"--back-output", (basename+"back.ngc").c_str(),
-		"--outline-output", (basename+"outline.ngc").c_str(),
-		"--drill-output", (basename+"drill.ngc").c_str()
+	const char *fake_basename_command_line[] = {
+		"",
+		("--front-output="+basename+"front.ngc").c_str(),
+		("--back-output="+basename+"back.ngc").c_str(),
+		("--outline-output="+basename+"outline.ngc").c_str(),
+		("--drill-output="+basename+"drill.ngc").c_str()
 	};
-	po::store(po::parse_command_line(9, (char**)fake_basename_command_line, generic, style), instance().vm);
+
+	po::store(po::parse_command_line(5, (char**)fake_basename_command_line, generic, style), instance().vm);
 	po::notify(instance().vm);
 }
 