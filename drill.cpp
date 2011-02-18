0e1e6dff299e8fc33d349a158931ea3c3067adb1
Patrick Birnzain <pbirnzain@users.sourceforge.net>
2011-02-18 14:33:03 +0100

    preamble and postamble cleanup

--- drill.cpp
+++ drill.cpp
@@ -44,13 +44,12 @@ ExcellonProcessor::ExcellonProcessor( string drillfile, const ivalue_t board_wid
 	strcpy(filename.get(), cfilename);
 
 	gerbv_open_layer_from_filename(project, filename.get());
-	if( project->file[0] == NULL)
-		throw drill_exception();
+	if( project->file[0] == NULL) throw drill_exception();
 
-	preamble=string("G94     ( Inches per minute feed rate. )\n")+
-	   "G20     ( Units == INCHES.             )\n"+
+	preamble = string("G94     ( Inches per minute feed rate. )\n") +
+	   "G20     ( Units == INCHES.             )\n" +
 	   "G90     ( Absolute coordinates.        )\n";
-	postamble=string("M9 ( Coolant off. )\n")+
+	postamble = string("M9 ( Coolant off. )\n") +
 		"M2 ( Program end. )\n\n";
 }
 
@@ -85,7 +84,7 @@ ExcellonProcessor::export_ngc( const string of_name, shared_ptr<Driller> driller
 	of << setw(7);
 
 	// preamble
-	of <<preamble
+	of << preamble
 	   << "S" << left << driller->speed << "  ( RPM spindle speed.           )\n"
 	   << endl;
 
--- main.cpp
+++ main.cpp
@@ -111,35 +111,32 @@ int main( int argc, char* argv[] )
 	}
 
 
-	string preamble="",postamble="";
-	preamble="G94     ( Inches per minute feed rate. )\nG20     ( Units == INCHES.             )\nG90     ( Absolute coordinates.        )\nM3      ( Spindle on clockwise.        )\n\n";
-	
-	postamble="M9 ( Coolant off. )\n M2 ( Program end. )\n\n";
+	string preamble, postamble;
 	
 	if( vm.count("preamble"))
 	{
-		string name=vm["preamble"].as<string>();
+		string name = vm["preamble"].as<string>();
 		fstream in(name.c_str(),fstream::in);
 		if(!in.good())
 		{
-			cerr<<"Cannot read preamble file \""<<name<<"\""<<endl;
+			cerr << "Cannot read preamble file \"" << name << "\"" << endl;
 			exit(1);
 		}
 		string tmp((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
-		preamble=tmp+"\n\n";
+		preamble = tmp + "\n\n";
 	}
 	
 	if( vm.count("postamble"))
 	{
-		string name=vm["postamble"].as<string>();
+		string name = vm["postamble"].as<string>();
 		fstream in(name.c_str(),fstream::in);
 		if(!in.good())
 		{
-			cerr<<"Cannot read preamble file \""<<name<<"\""<<endl;
+			cerr << "Cannot read preamble file \"" << name << "\"" << endl;
 			exit(1);
 		}
 		string tmp((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
-		postamble=tmp+"\n\n";
+		postamble = tmp + "\n\n";
 	}
 	
 	shared_ptr<Board> board( new Board( vm["dpi"].as<int>()) );
@@ -202,8 +199,8 @@ int main( int argc, char* argv[] )
 
 		shared_ptr<NGC_Exporter> exporter( new NGC_Exporter( board ) );
 		exporter->add_header( PACKAGE_STRING );
-		exporter->set_preamble(preamble);
-		exporter->set_postamble(postamble);
+		if( vm.count("preamble") ) exporter->set_preamble(preamble);
+		if( vm.count("postamble") ) exporter->set_postamble(postamble);
 		exporter->export_all(vm);
 	} catch( std::logic_error& le ) {
 		cout << "Internal Error: " << le.what() << endl;
@@ -215,8 +212,8 @@ int main( int argc, char* argv[] )
 		try {
 			ExcellonProcessor ep( vm["drill"].as<string>(), board->get_min_x() + board->get_max_x() );
 			ep.add_header( PACKAGE_STRING );
-			ep.set_preamble(preamble);
-			ep.set_postamble(postamble);
+			if( vm.count("preamble") ) ep.set_preamble(preamble);
+			if( vm.count("postamble") ) ep.set_postamble(postamble);
 			
 			if( vm.count("milldrill") )
 				ep.export_ngc( vm["drill-output"].as<string>(), cutter, !vm.count("drill-front"), vm.count("mirror-absolute") );
