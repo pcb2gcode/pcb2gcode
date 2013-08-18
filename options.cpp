96f24fb94c7fdc52290308afc0faee4c557a6ee1
erik74 <erik@muenchen-ist-toll.de>
2013-08-18 16:23:10 +0200

    Added "cut-front" option to command line parameters.

--- options.cpp
+++ options.cpp
@@ -4,6 +4,10 @@
  \file       options.cpp
  \brief
 
+\version
+ 18.08.2013 - Erik Schuster - erik@muenchen-ist-toll.de\n
+ - Added cut-front option.
+
  \version
  04.08.2013 - Erik Schuster - erik@muenchen-ist-toll.de\n
  - Added metricoutput option.
@@ -146,77 +150,58 @@ void options::parse_files() {
 /*
  */
 /******************************************************************************/
-options::options() :
-		cli_options("command line only options"), cfg_options(
-				"generic options (CLI and config files)") {
-	cli_options.add_options()("help,?", "produce help message")("version",
-			"\n");
+options::options() :	cli_options("command line only options"), cfg_options("generic options (CLI and config files)") {
 
-	cfg_options.add_options()("front", po::value<string>(),
-			"front side RS274-X .gbr")("back", po::value<string>(),
-			"back side RS274-X .gbr")("outline", po::value<string>(),
-			"pcb outline polygon RS274-X .gbr")("drill", po::value<string>(),
-			"Excellon drill file\n")
+	cli_options.add_options()
+	("help,?", "produce help message")
+	("version","\n");
+
+	cfg_options.add_options()
+	("front", po::value<string>(), "front side RS274-X .gbr")
+	("back", po::value<string>(), "back side RS274-X .gbr")
+	("outline", po::value<string>(), "pcb outline polygon RS274-X .gbr")
+	("drill", po::value<string>(), "Excellon drill file\n")
 
 	("svg", po::value<string>(), "SVG output file. EXPERIMENTAL\n")
 
-	("zwork", po::value<double>(),
-			"milling depth in inches (Z-coordinate while engraving)")("zsafe",
-			po::value<double>(),
-			"safety height (Z-coordinate during rapid moves)")("offset",
-			po::value<double>(),
-			"distance between the PCB traces and the end mill path in inches; usually half the isolation width")(
-			"mill-feed", po::value<double>(), "feed while isolating in ipm")(
-			"mill-speed", po::value<int>(), "spindle rpm when milling")(
-			"milldrill", "drill using the mill head")("extra-passes",
-			po::value<int>(),
-			"specify the the number of extra isolation passes, increasing the isolation width half the tool diameter with each pass\n")
+	("zwork", po::value<double>(), "milling depth in inches (Z-coordinate while engraving)")
+	("zsafe", po::value<double>(), 	"safety height (Z-coordinate during rapid moves)")
+	("offset", po::value<double>(), "distance between the PCB traces and the end mill path in inches; usually half the isolation width")
+	("mill-feed", po::value<double>(), "feed while isolating in ipm")
+	("mill-speed", po::value<int>(), "spindle rpm when milling")
+	("milldrill", "drill using the mill head")
+	("extra-passes", po::value<int>(), "specify the the number of extra isolation passes, increasing the isolation width half the tool diameter with each pass\n")
 
-	("fill-outline", po::value<bool>()->zero_tokens(),
-			"accept a contour instead of a polygon as outline")("outline-width",
-			po::value<double>(), "width of the outline")("cutter-diameter",
-			po::value<double>(),
-			"diameter of the end mill used for cutting out the PCB")("zcut",
-			po::value<double>(), "PCB cutting depth in inches.")("cut-feed",
-			po::value<double>(), "PCB cutting feed")("cut-speed",
-			po::value<int>(), "PCB cutting spindle speed")("cut-infeed",
-			po::value<double>(),
-			"Maximum cutting depth; PCB may be cut in multiple passes\n")
+	("fill-outline", po::value<bool>()->zero_tokens(), "accept a contour instead of a polygon as outline")
+	("outline-width", po::value<double>(), "width of the outline")
+	("cutter-diameter",	po::value<double>(), "diameter of the end mill used for cutting out the PCB")
+	("zcut", po::value<double>(), "PCB cutting depth in inches")
+	("cut-feed", po::value<double>(), "PCB cutting feed")
+	("cut-speed", po::value<int>(), "PCB cutting spindle speed")
+	("cut-infeed", po::value<double>(), "Maximum cutting depth; PCB may be cut in multiple passes")
+	("cut-front", po::value<bool>()->zero_tokens(), "Cut from front side. Default is back side.\n")
 
-	("zdrill", po::value<double>(), "drill depth")("zchange",
-			po::value<double>(), "tool changing height")("drill-feed",
-			po::value<double>(), "drill feed; ipm")("drill-speed",
-			po::value<int>(), "spindle rpm when drilling")("drill-front",
-			po::value<bool>()->zero_tokens(),
-			"drill through the front side of board")("onedrill",
-			po::value<bool>()->default_value(false)->zero_tokens()->implicit_value(
-					true), "use only one drill bit size\n")
+	("zdrill", po::value<double>(), "drill depth")
+	("zchange",	po::value<double>(), "tool changing height")
+	("drill-feed", po::value<double>(), "drill feed; ipm")
+	("drill-speed",	po::value<int>(), "spindle rpm when drilling")
+	("drill-front",	po::value<bool>()->zero_tokens(), "drill through the front side of board")
+	("onedrill", po::value<bool>()->default_value(false)->zero_tokens()->implicit_value(true), "use only one drill bit size\n")
 
-	("metric",
-			po::value<bool>()->default_value(false)->zero_tokens()->implicit_value(
-					true),
-			"use metric units for parameters. does not affect gcode output")(
-			"metricoutput",
-			po::value<bool>()->default_value(false)->zero_tokens()->implicit_value(
-					true), "use metric units for output")("dpi",
-			po::value<int>()->default_value(1000),
-			"virtual photoplot resolution")("g64", po::value<double>(),
-			"maximum deviation from toolpath")("mirror-absolute",
-			po::value<bool>()->zero_tokens(),
-			"mirror back side along absolute zero instead of board center\n")
+	("metric", po::value<bool>()->default_value(false)->zero_tokens()->implicit_value(	true), "use metric units for parameters. does not affect gcode output")
+	("metricoutput", po::value<bool>()->default_value(false)->zero_tokens()->implicit_value(true), "use metric units for output")
+	("dpi",	po::value<int>()->default_value(1000), "virtual photoplot resolution")
+	("g64", po::value<double>(), "maximum deviation from toolpath")
+	("mirror-absolute",	po::value<bool>()->zero_tokens(), "mirror back side along absolute zero instead of board center\n")
 
-	("basename", po::value<string>(), "prefix for default output file names")(
-			"front-output", po::value<string>()->default_value("front.ngc"),
-			"output file for front layer")("back-output",
-			po::value<string>()->default_value("back.ngc"),
-			"output file for back layer")("outline-output",
-			po::value<string>()->default_value("outline.ngc"),
-			"output file for outline")("drill-output",
-			po::value<string>()->default_value("drill.ngc"),
-			"output file for drilling\n")
+	("basename", po::value<string>(), "prefix for default output file names")
+	("front-output", po::value<string>()->default_value("front.ngc"), "output file for front layer")
+	("back-output",	po::value<string>()->default_value("back.ngc"), "output file for back layer")
+	("outline-output", po::value<string>()->default_value("outline.ngc"), "output file for outline")
+	("drill-output", po::value<string>()->default_value("drill.ngc"), "output file for drilling\n")
 
-	("preamble", po::value<string>(), "gcode preamble file")("postamble",
-			po::value<string>(), "gcode postamble file");
+	("preamble", po::value<string>(), "gcode preamble file")
+	("postamble", po::value<string>(), "gcode postamble file");
 }
 
 /******************************************************************************/
