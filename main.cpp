3b2d8f8ed52b12719005edd11adb2c8c05cb1069
Nicola Corna <nicola@corna.info>
2016-09-30 15:21:08 +0200

    Add --spinup-time and --spindown-time

--- main.cpp
+++ main.cpp
@@ -91,6 +91,8 @@ int main(int argc, char* argv[])
     const double tolerance = vm["tolerance"].as<double>() * unit;
     const bool explicit_tolerance = !vm["nog64"].as<bool>();
     const string outputdir = vm["output-dir"].as<string>();
+    const double spindown_time = vm.count("spindown-time") ?
+        vm["spindown-time"].as<double>() : vm["spinup-time"].as<double>();
     shared_ptr<Isolator> isolator;
 
     if (vm.count("front") || vm.count("back"))
@@ -115,6 +117,8 @@ int main(int argc, char* argv[])
         isolator->tolerance = tolerance;
         isolator->explicit_tolerance = explicit_tolerance;
         isolator->mirror_absolute = vm["mirror-absolute"].as<bool>();
+        isolator->spinup_time = vm["spinup-time"].as<double>();
+        isolator->spindown_time = spindown_time;
     }
 
     shared_ptr<Cutter> cutter;
@@ -139,6 +143,8 @@ int main(int argc, char* argv[])
         cutter->tolerance = tolerance;
         cutter->explicit_tolerance = explicit_tolerance;
         cutter->mirror_absolute = vm["mirror-absolute"].as<bool>();
+        cutter->spinup_time = vm["spinup-time"].as<double>();
+        cutter->spindown_time = spindown_time;
         cutter->bridges_num = vm["bridgesnum"].as<unsigned int>();
         cutter->bridges_width = vm["bridges"].as<double>() * unit;
         if (vm.count("zbridges"))
@@ -159,6 +165,8 @@ int main(int argc, char* argv[])
         driller->tolerance = tolerance;
         driller->explicit_tolerance = explicit_tolerance;
         driller->mirror_absolute = vm["mirror-absolute"].as<bool>();
+        driller->spinup_time = vm["spinup-time"].as<double>();
+        driller->spindown_time = spindown_time;
         driller->zchange = vm["zchange"].as<double>() * unit;
     }
 
