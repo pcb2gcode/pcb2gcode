e4c11ee55228e0b5aa6867c05a3ec072092892fe
Eyal Soha <eyalsoha@gmail.com>
2017-12-14 20:50:22 +0200

    fix up the extra passes

--- main.cpp
+++ main.cpp
@@ -96,7 +96,11 @@ int main(int argc, char* argv[])
     if (vm.count("front") || vm.count("back"))
     {
         isolator = shared_ptr<Isolator>(new Isolator());
-        isolator->tool_diameter = vm["offset"].as<double>() * 2 * unit;
+        if (vm.count("offset")) {
+            isolator->tool_diameter = vm["offset"].as<double>() * 2 * unit;
+        } else {
+            isolator->tool_diameter = -1;
+        }
         isolator->zwork = vm["zwork"].as<double>() * unit;
         isolator->zsafe = vm["zsafe"].as<double>() * unit;
         isolator->feed = vm["mill-feed"].as<double>() * unit;
--- options.cpp
+++ options.cpp
@@ -475,6 +475,11 @@ static void check_milling_parameters(po::variables_map const& vm)
                 cerr << "Error: --voronoi requires an outline.\n";
                 exit(ERR_VORONOINOOUTLINE);
             }
+
+            if (vm["extra-passes"].as<int>() > 0 && !vm.count("offset")) {
+                cerr << "Error: Engraving --offset not specified but required for extra-passes.\n";
+                exit(ERR_NOOFFSET);
+            }
         }
         else
         {
