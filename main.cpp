3bde2733132f2c306629ed2deb89e2d06ca81097
Eyal <eyalsoha@gmail.com>
2018-02-01 15:42:15 +0200

    Fix mistake in eulerian paths, assigned to isolator instead of cutter (#94)

--- main.cpp
+++ main.cpp
@@ -135,7 +135,7 @@ int main(int argc, char* argv[])
         cutter->do_steps = true;
         cutter->stepsize = vm["cut-infeed"].as<double>() * unit;
         cutter->optimise = vm["optimise"].as<bool>();
-        isolator->eulerian_paths = vm["eulerian-paths"].as<bool>();
+        cutter->eulerian_paths = vm["eulerian-paths"].as<bool>();
         cutter->tolerance = tolerance;
         cutter->explicit_tolerance = explicit_tolerance;
         cutter->mirror_absolute = vm["mirror-absolute"].as<bool>();
