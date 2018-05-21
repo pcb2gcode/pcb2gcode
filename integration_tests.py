018bafd488e834da1dc6b39e5a290458e84a7eb7
Eyal <eyalsoha@gmail.com>
2018-05-21 14:28:27 +0300

    Add x-offset and y-offset options (#142)
    * Add x y offset options
    * Exit on failure to open an input file and break integration tests on failure to run
    * Add coverage by adding support for integration tests that fail

--- integration_tests.py
+++ integration_tests.py
@@ -10,16 +10,26 @@ import filecmp
 import sys
 import argparse
 import re
+import collections
+
+TestCase = collections.namedtuple("TestCase", ["input_path", "args", "exit_code"])
 
 EXAMPLES_PATH = "testing/gerbv_example"
-TEST_CASES = [os.path.join(EXAMPLES_PATH, x)
-              for x in ("multivibrator",
-                        "am-test-voronoi",
-                        "slots-milldrill")]
+TEST_CASES = ([TestCase(os.path.join(EXAMPLES_PATH, x), [], 0)
+              for x in [
+                  "multivibrator",
+                  "am-test-voronoi",
+                  "slots-milldrill",
+                  "multivibrator_xy_offset",
+              ]] +
+              [TestCase(os.path.join(EXAMPLES_PATH, "multivibrator"), ["--front=non_existant_file"], 1),
+               TestCase(os.path.join(EXAMPLES_PATH, "multivibrator"), ["--back=non_existant_file"], 1),
+               TestCase(os.path.join(EXAMPLES_PATH, "multivibrator"), ["--outline=non_exsistant_file"], 1),
+              ])
 
 class IntegrationTests(unittest.TestCase):
 
-  def pcb2gcode_one_directory(self, input_path):
+  def pcb2gcode_one_directory(self, input_path, args=[], exit_code=0):
     """Run pcb2gcode once in one directory.
 
     Current working directory remains unchanged at the end.
@@ -31,7 +41,9 @@ class IntegrationTests(unittest.TestCase):
     pcb2gcode = os.path.join(cwd, "pcb2gcode")
     os.chdir(input_path)
     actual_output_path = tempfile.mkdtemp()
-    subprocess.call([pcb2gcode, "--output-dir", actual_output_path])
+    self.assertEqual(
+        subprocess.call([pcb2gcode, "--output-dir", actual_output_path] + args),
+        exit_code)
     os.chdir(cwd)
     return actual_output_path
 
@@ -94,7 +106,7 @@ class IntegrationTests(unittest.TestCase):
         all_diffs += difflib.unified_diff(data0, data1, os.path.join(left_prefix, f), os.path.join(right_prefix, f))
     return ''.join(all_diffs)
 
-  def run_one_directory(self, input_path, expected_output_path, test_prefix):
+  def run_one_directory(self, input_path, expected_output_path, test_prefix, args=[], exit_code=0):
     """Run pcb2gcode on a directory and return the diff as a string.
 
     Returns an empty string if there is no mismatch.
@@ -103,7 +115,9 @@ class IntegrationTests(unittest.TestCase):
     expected_output_path: Path to expected outputs
     test_prefix: Strin to prepend to all filenamess
     """
-    actual_output_path = self.pcb2gcode_one_directory(input_path)
+    actual_output_path = self.pcb2gcode_one_directory(input_path, args, exit_code)
+    if exit_code:
+      return ""
     diff_text = self.compare_directories(expected_output_path, actual_output_path,
                                          os.path.join("expected", test_prefix),
                                          os.path.join("actual", test_prefix))
@@ -115,10 +129,10 @@ class IntegrationTests(unittest.TestCase):
     test_cases = TEST_CASES
     diff_texts = []
     for test_case in test_cases:
-      test_prefix = os.path.join(test_case, "expected")
-      input_path = os.path.join(cwd, test_case)
-      expected_output_path = os.path.join(cwd, test_case, "expected")
-      diff_texts.append(self.run_one_directory(input_path, expected_output_path, test_prefix))
+      test_prefix = os.path.join(test_case.input_path, "expected")
+      input_path = os.path.join(cwd, test_case.input_path)
+      expected_output_path = os.path.join(cwd, test_case.input_path, "expected")
+      diff_texts.append(self.run_one_directory(input_path, expected_output_path, test_prefix, test_case.args, test_case.exit_code))
     self.assertFalse(any(diff_texts),
                      'Files don\'t match\n' + '\n'.join(diff_texts) +
                      '\n***\nRun one of these:\n' +
