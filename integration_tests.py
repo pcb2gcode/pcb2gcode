ef6cc00e43d8cbb739fb8010fdc2d27b7542e9f5
Eyal Soha <eyalsoha@gmail.com>
2019-01-04 23:28:46 -0500

    Add edge-cuts-inside-cuts test case for better coverage

--- integration_tests.py
+++ integration_tests.py
@@ -29,6 +29,7 @@ TEST_CASES = ([TestCase(clean(x), os.path.join(EXAMPLES_PATH, x), [], 0)
                   "am-test-extended",
                   "am-test-voronoi",
                   "am-test-voronoi-front",
+                  "edge-cuts-inside-cuts",
                   "example_board_al_custom",
                   "example_board_al_linuxcnc",
                   "KNoT-Gateway Mini Starter Board",
