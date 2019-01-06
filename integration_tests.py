2c745c336e51ca4a6ede967305db302253d5d3ef
Eyal Soha <eyalsoha@gmail.com>
2019-01-06 08:00:45 -0500

    Add mill_masking_voronoi to integration tests

--- integration_tests.py
+++ integration_tests.py
@@ -39,6 +39,7 @@ TEST_CASES = ([TestCase(clean(x), os.path.join(EXAMPLES_PATH, x), [], 0)
                   "KNoT-Gateway Mini Starter Board",
                   "KNoT_Thing_Starter_Board",
                   "mill_masking",
+                  "mill_masking_voronoi",
                   "milldrilldiatest",
                   "multivibrator",
                   "multivibrator-basename",
