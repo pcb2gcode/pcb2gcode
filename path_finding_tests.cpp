a91562033c697b30c616bfdd27a14ea8fee62209
Eyal Soha <eyalsoha@gmail.com>
2019-02-11 15:57:08 -0500

    Add a keep_in example called u_shape

--- path_finding_tests.cpp
+++ path_finding_tests.cpp
@@ -103,4 +103,30 @@ BOOST_AUTO_TEST_CASE(reuse_surface) {
   BOOST_CHECK_EQUAL(ret, make_optional(expected));
 }
 
+BOOST_AUTO_TEST_CASE(u_shape) {
+  ring_type_fp u_shape;
+  u_shape.push_back(point_type_fp( 0, 10));
+  u_shape.push_back(point_type_fp( 3, 10));
+  u_shape.push_back(point_type_fp( 3,  3));
+  u_shape.push_back(point_type_fp( 7,  3));
+  u_shape.push_back(point_type_fp( 7, 10));
+  u_shape.push_back(point_type_fp(10, 10));
+  u_shape.push_back(point_type_fp(10,  0));
+  u_shape.push_back(point_type_fp( 0,  0));
+  u_shape.push_back(point_type_fp( 0, 10));
+  polygon_type_fp poly;
+  poly.outer() = u_shape;
+  multi_polygon_type_fp keep_in;
+  keep_in.push_back(poly);
+  auto surface = create_path_finding_surface(boost::make_optional(keep_in), boost::none, 0.1);
+  auto ret = find_path(surface, point_type_fp(1,9), point_type_fp(9,9));
+
+  linestring_type_fp expected;
+  expected.push_back(point_type_fp(1, 9));
+  expected.push_back(point_type_fp(3, 3));
+  expected.push_back(point_type_fp(7, 3));
+  expected.push_back(point_type_fp(9, 9));
+  BOOST_CHECK_EQUAL(ret, make_optional(expected));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
