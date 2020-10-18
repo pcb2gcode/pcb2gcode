803d67c323c9b16494547b6d669d88c0f104c48e
eyal0 <109809+eyal0@users.noreply.github.com>
2020-10-17 19:16:31 -0600

    Only output gcode end after the last tool
    This fixes #475

--- ngc_exporter.cpp
+++ ngc_exporter.cpp
@@ -318,10 +318,12 @@ void NGC_Exporter::export_layer(shared_ptr<Layer> layer, string of_name, boost::
         continue; // Nothing to do for this mill size.
       }
       Tiling tiling(tileInfo, cfactor, main_sub_ocodes.getUniqueCode());
-      tiling.setGCodeEnd(string("\nG04 P0 ( dwell for no time -- G64 should not smooth over this point )\n")
-                         + (bZchangeG53 ? "G53 " : "") + "G00 Z" + str( format("%.6f") % ( mill->zchange * cfactor ) ) +
-                         " ( retract )\n\n" + postamble + "M5 ( Spindle off. )\nG04 P" +
-                         to_string(mill->spindown_time) + "\n");
+      if (toolpaths_index == all_toolpaths.size() - 1) {
+        tiling.setGCodeEnd(string("\nG04 P0 ( dwell for no time -- G64 should not smooth over this point )\n")
+                           + (bZchangeG53 ? "G53 " : "") + "G00 Z" + str( format("%.6f") % ( mill->zchange * cfactor ) ) +
+                           " ( retract )\n\n" + postamble + "M5 ( Spindle off. )\nG04 P" +
+                           to_string(mill->spindown_time) + "\n");
+      }
 
       // Start the new tool.
       of << endl
