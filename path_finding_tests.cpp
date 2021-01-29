9421e60266acfde3019d7da54c64187006b8454b
eyal0 <109809+eyal0@users.noreply.github.com>
2021-01-29 13:13:25 -0700

    Use a faster and more accurate intersection algorithm

--- path_finding_tests.cpp
+++ path_finding_tests.cpp
@@ -17,6 +17,83 @@ using namespace std;
 using namespace path_finding;
 
 BOOST_AUTO_TEST_SUITE(path_finding_tests)
+BOOST_AUTO_TEST_SUITE(is_intersecting_tests)
+
+// From: https://stackoverflow.com/a/14409947/4454
+class Adler32 {
+ public:
+  void add(char x) {
+    s1 = (s1 + x) % 65521;
+    s2 = (s2 + s1) % 65521;
+  }
+  uint32_t get() {
+    return (s2 << 16) | s1;
+  }
+
+ private:
+  uint32_t s1 = 1;
+  uint32_t s2 = 0;
+};
+
+BOOST_AUTO_TEST_CASE(big) {
+  Adler32 hasher;
+  for (double x0 = -4; x0 < 4; x0++) {
+    for (double y0 = -4; y0 < 4; y0++) {
+      for (double x1 = -4; x1 < 4; x1++) {
+        for (double y1 = -4; y1 < 4; y1++) {
+          for (double x2 = -4; x2 < 4; x2++) {
+            for (double y2 = -4; y2 < 4; y2++) {
+              for (double x3 = -4; x3 < 4; x3++) {
+                for (double y3 = -4; y3 < 4; y3++) {
+                  point_type_fp p0{x0, y0};
+                  point_type_fp p1{x1, y1};
+                  point_type_fp p2{x2, y2};
+                  point_type_fp p3{x3, y3};
+                  linestring_type_fp ls0{p0,p1};
+                  linestring_type_fp ls1{p2,p3};
+                  hasher.add(is_intersecting(p0, p1, p2, p3));
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  BOOST_CHECK_EQUAL(hasher.get(), 4000232678);
+}
+
+// This test is slow but it can be used once to confirm the results of the test above.
+BOOST_AUTO_TEST_CASE(small, *boost::unit_test::disabled()) {
+  for (double x0 = -3; x0 < 3; x0++) {
+    for (double y0 = -3; y0 < 3; y0++) {
+      for (double x1 = -3; x1 < 3; x1++) {
+        for (double y1 = -3; y1 < 3; y1++) {
+          for (double x2 = -3; x2 < 3; x2++) {
+            for (double y2 = -3; y2 < 3; y2++) {
+              for (double x3 = -3; x3 < 3; x3++) {
+                for (double y3 = -3; y3 < 3; y3++) {
+                  point_type_fp p0{x0, y0};
+                  point_type_fp p1{x1, y1};
+                  point_type_fp p2{x2, y2};
+                  point_type_fp p3{x3, y3};
+                  linestring_type_fp ls0{p0,p1};
+                  linestring_type_fp ls1{p2,p3};
+                  BOOST_TEST_INFO("intersection: " << bg::wkt(ls0) << " " << bg::wkt(ls1));
+                  BOOST_CHECK_EQUAL(
+                      path_finding::is_intersecting(p0, p1, p2, p3),
+                      bg::intersects(ls0, ls1));
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_CASE(simple) {
   box_type_fp bounding_box = bg::return_envelope<box_type_fp>(point_type_fp(-100, -100));
