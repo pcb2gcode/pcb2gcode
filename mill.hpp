12d80ca5cc20b4dfa2706826e402a470defbd206
eyal0 <109809+eyal0@users.noreply.github.com>
2021-02-22 11:30:11 -0700

    refactor: Add better comments to mill.hpp

--- mill.hpp
+++ mill.hpp
@@ -21,53 +21,44 @@
 #ifndef MILL_H
 #define MILL_H
 
-#include <stdint.h>
-#include <string.h>
+#include <string>
 #include <vector>
 
-/******************************************************************************/
-/*
- */
-/******************************************************************************/
+// All tools are mills.
 class Mill {
  public:
   virtual ~Mill() {}
 
-  double feed;
-  double vertfeed;
-  int speed;
-  double zchange;
-  double zsafe;
-  double zwork;
-  double tolerance;
-  bool explicit_tolerance;
-  bool backside;
-  double spinup_time;
-  double spindown_time;
-  std::string pre_milling_gcode;
-  std::string post_milling_gcode;
+  double feed;  // Horizontal speed.
+  double vertfeed; // Vertical speed.
+  int speed; // Rotational speed.
+  double zchange; // Height at which to change tools.
+  double zsafe; // Height at which it is always safe to move around.
+  double zwork; // Height at which milling is done.
+  double tolerance; // small value to use as an epsilon everwhere.
+  bool explicit_tolerance; // Should we g64 or not?
+  bool backside; // Is this tool being used on the back of the board?
+  double spinup_time; // How much time to allow the tool to get up to spinnig speed?
+  double spindown_time; // How much time to allow the tool to stop rotating?
+  std::string pre_milling_gcode; // gcode to insert before using this tool
+  std::string post_milling_gcode; // gcode to insert after using this tool
 };
 
-/******************************************************************************/
-/*
- */
-/******************************************************************************/
+// A routing mill is one that draws paths, either for cutting the PCB
+// or for etching a line in it.
 class RoutingMill: public Mill {
  public:
-  double optimise;
-  bool eulerian_paths;
-  size_t path_finding_limit;
-  double g0_vertical_speed;
-  double g0_horizontal_speed;
-  double backtrack;
-  double stepsize;
+  double optimise; // Should we optimize this path with Douglas-Peucker algorithm?
+  bool eulerian_paths; // Should we combine paths using eulerian walk?
+  size_t path_finding_limit; // How much effort to make in path finding?
+  double g0_vertical_speed; // How fast is the CNC able to make vertical moves?
+  double g0_horizontal_speed; // How fast is the CNC able to make horizontal moves?
+  double backtrack; // How much additional milling are we willing to do to save some time?
+  double stepsize; // When milling deeply, what is the maximum depth of each pass?
   double offset;  // Stay away from the traces by this amount.
 };
 
-/******************************************************************************/
-/*
- */
-/******************************************************************************/
+// An isolator etches a line into the PCB to separate copper regions.
 class Isolator: public RoutingMill {
  public:
   // Each element is the tool diameter and the overlap width for that tool, both
@@ -79,10 +70,7 @@ class Isolator: public RoutingMill {
   double isolation_width;
 };
 
-/******************************************************************************/
-/*
- */
-/******************************************************************************/
+// A cutter cuts the PCB.
 class Cutter: public RoutingMill {
  public:
   double tool_diameter;
@@ -91,12 +79,10 @@ class Cutter: public RoutingMill {
   double bridges_width;
 };
 
-/******************************************************************************/
-/*
- */
-/******************************************************************************/
-class Driller: public Mill
-{
+// A driller only plunges and comes back up, for making holes in the
+// PCB.
+class Driller: public Mill {
+
 };
 
 #endif // MILL_H
