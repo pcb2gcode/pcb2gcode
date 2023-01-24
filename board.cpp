fc33e4143bbb890068be9d2e116f96b15b6e5913
eyal0 <109809+eyal0@users.noreply.github.com>
2023-01-24 08:54:10 -0700

    fix: Account for the overlap of the isolator in tiling.
    When tiling, the isolator will try to overlap the cutter in order to remove burrs.  Account for the overlap.

--- board.cpp
+++ board.cpp
@@ -89,7 +89,22 @@ void Board::createLayers()
       shared_ptr<Cutter> outline_mill = static_pointer_cast<Cutter>(get<1>(outline->second));
       const auto& importer = get<0>(outline->second);
       coordinate_type_fp tool_diameter = outline_mill->tool_diameter;
-      bounding_box = bg::return_buffer<box_type_fp>(importer->get_bounding_box(), tool_diameter);
+
+      // The milling will try to extend beyond the cutting tool in
+      // order to remove burrs so we account for that overlap, too.
+      coordinate_type_fp max_overlap = 0;
+      for (const auto& layer_name : std::vector<std::string>{"front", "back"}) {
+        const auto current_layer = prepared_layers.find(layer_name);
+        if (current_layer != prepared_layers.cend()) {
+          shared_ptr<Isolator> trace_mill = static_pointer_cast<Isolator>(get<1>(current_layer->second));
+          for (const auto& tool : trace_mill->tool_diameters_and_overlap_widths) {
+            auto overlap_width = tool.second;
+            max_overlap = std::max(max_overlap, overlap_width);
+          }
+        }
+      }
+      bounding_box = bg::return_buffer<box_type_fp>(importer->get_bounding_box(),
+                                                    tool_diameter + max_overlap);
     } else {
       for (const auto& layer_name : std::vector<std::string>{"front", "back"}) {
         const auto current_layer = prepared_layers.find(layer_name);
