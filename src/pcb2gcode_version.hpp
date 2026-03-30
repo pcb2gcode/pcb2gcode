dd3eb536f6db958fe0afd7d4d35e1e639a05875f
eyal0 <109809+eyal0@users.noreply.github.com>
2026-03-29 20:23:50 -0600

    refactor: Combine all the generated cmake into one file.

--- src/pcb2gcode_version.hpp
+++ src/pcb2gcode_version.hpp
@@ -1,10 +1,18 @@
 #ifndef PCB2GCODE_VERSION_HPP
 #define PCB2GCODE_VERSION_HPP
 
-#if __has_include("pcb2gcode_version_generated.hpp")
-#include "pcb2gcode_version_generated.hpp"
+#if __has_include("config.h")
+#include "config.h"
 #else
 #define PCB2GCODE_PROJECT_VERSION "unknown"
+#define PACKAGE "pcb2gcode"
+#define PACKAGE_BUGREPORT "unknown"
+#define PACKAGE_NAME "pcb2gcode"
+#define PACKAGE_STRING "pcb2gcode unknown"
+#define PACKAGE_TARNAME "pcb2gcode"
+#define PACKAGE_URL "unknown"
+#define PACKAGE_VERSION "unknown"
+#define VERSION "unknown"
 #endif
 
 #endif // PCB2GCODE_VERSION_HPP
